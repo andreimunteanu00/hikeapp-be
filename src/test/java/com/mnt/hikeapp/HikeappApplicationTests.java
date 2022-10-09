82aac67ca8adc51eeca06f938993c7f99b32a1e1
andrei.munteanu <andrei.munteanu@snt.ro>
2022-10-09 13:28:49 +0300

    feat: init be project

---
+++ src/test/java/com/mnt/hikeapp/HikeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnt.hikeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
